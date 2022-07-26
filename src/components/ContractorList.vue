d84f5a9ef5d214edc708b3d096e1e805cb928d14
vladimir <grigorevvladimir1999@gmail.com>
2022-07-27 00:37:24 +0300

    adding contractor list

---
+++ src/components/ContractorList.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-data-table
+      :headers="headers"
+      :items="contractors"
+      :items-per-page="50"
+      class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: "ContractorList",
+  props: {
+    contractors: {
+      type: Array,
+      required: true
+    },
+    headers: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.v-data-table {
+  border: 1px solid #ccc;
+}
+.v-data-table > .v-data-table__wrapper > table > tbody > tr > td,
+.v-data-table > .v-data-table__wrapper > table > thead > tr > td,
+.v-data-table > .v-data-table__wrapper > table > tfoot > tr > td {
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif;;
+  font-size: 12px;
+}
+.v-data-table > .v-data-table__wrapper > table > thead {
+  background-color: #5ea97c;
+}
+.v-data-table > .v-data-table__wrapper > table > thead > tr > th{
+  color: white !important;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ src/pages/ContractorListPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container fluid>
+    <h1>Контрагенты</h1>
+    <ContractorList
+        :headers="headers"
+        :contractors="contractors"
+    />
+  </v-container>
+</template>
+
+<script>
+import ContractorList from "@/components/ContractorList";
+import axios from "axios";
+
+export default {
+  name: "ContractorListPage",
+  components: {
+    ContractorList
+  },
+  data() {
+    return {
+      headers: [
+        {text: 'Обозначение', value: 'lbl'},
+        {text: 'Полное название', value: 'nameFull'},
+        {text: 'Юридический адрес', value: 'address'},
+        {text: 'ИНН', value: 'inn'},
+        {text: 'КПП', value: 'kpp'},
+        {text: 'Укрупненный перечень товаров/услуг', value: 'listWork'},
+      ],
+      contractors: [],
+      page: 0,
+      size: 50,
+    }
+  },
+  methods: {
+    async fetchContractors() {
+      try {
+        const response = await axios.post(
+            `https://open-etp-backend.inlinegroup-c.ru/Open-ETP/contractor/filter/?page=${this.page}&size=${this.size}`
+        );
+        this.contractors = response.data.content;
+      } catch (e) {
+        alert('Ошибка')
+      }
+    }
+  },
+  mounted() {
+    this.fetchContractors();
+  },
+}
+</script>
+
+<style>
+h1 {
+  margin: 0 0 5px 0;
+  color: #42b983;
+  font-size: 16px;
+}
+</style>
